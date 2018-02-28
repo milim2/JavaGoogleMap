90715924988a8c6b3792034b7a2a8db6785425a8
Seizy <irene33m@gmail.com>
2018-02-28 02:14:36 -0500

    Java Project1-main page

---
+++ Documents/JAVA_Project/LectureEvaluation/src/java/user/userDAO.java
@@ -0,0 +1,238 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package user;
+
+import java.sql.Connection;
+import util.DatabaseUtil;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author irene
+ */
+public class userDAO {
+
+    public int login(String userID, String userPassword) {
+        String SQL = "SELECT userPassword FROM USER WHERE userID = ?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = DatabaseUtil.getConnection();
+            pstmt = conn.prepareStatement(SQL);
+            pstmt.setString(1, userID);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                if (rs.getString(1).equals(userPassword)) {
+                    return 1; // Succeed login
+                } else {
+                    return 0; // Wrong password
+                }
+            }
+            return -1; // No ID            
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return -2; // Error on DB    
+        }
+        
+    
+
+    public int join(UserDTO user){
+        String SQL = "INSERT INTO USER VALUES (?, ?, ?, ?, false)";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = DatabaseUtil.getConnection();
+            pstmt = conn.prepareStatement(SQL);
+            pstmt.setString(1, user.getUserID());
+            pstmt.setString(2, user.getUserPassword());
+            pstmt.setString(3, user.getUserEmail());
+            pstmt.setString(4, user.getUserEmailHash());
+            return pstmt.exeuteUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return -1; // Fail to register
+    }
+    
+
+    public String getUserEmail(String userID) {
+        String SQL = "SELECT userEmail FROM USER WHERE userID = ?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = DatabaseUtil.getConnection();
+            pstmt = conn.prepareStatement(SQL);
+            pstmt.setString(1, userID);
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                return rs.getString(1); // 
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return null; // DB error  
+    }
+
+    public boolean getUserEmailChecked(String userID) {
+        String SQL = "SELECT userEmailChecked FROM USER WHERE userID = ?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = DatabaseUtil.getConnection();
+            pstmt = conn.prepareStatement(SQL);
+            pstmt.setString(1, userID);
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                return rs.getBoolean(1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return false; // DB error  
+    }
+
+    // email confirm
+    public boolean setUserEmailChecked(String userID) {
+        String SQL = "UPDATE USER SET userEmailChecked = true WHERE userID = ?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = DatabaseUtil.getConnection();
+            pstmt = conn.prepareStatement(SQL);
+            pstmt.setString(1, userID);
+            pstmt.exeuteUpdate();
+            return true; // continuously be able to login            
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return false; // DB error     
+    }
+
+}
+
---
+++ Documents/JAVA_Project/LectureEvaluation/src/java/util/DatabaseUtil.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author irene
+ */
+public class DatabaseUtil {
+    
+    public static Connection getConnection(){
+        try{
+            String dbURL = "jdbc:mysql://localhost:3306/LectureEvaluation";
+            String dbID = "root";
+            String dbPassword = "root";
+            Class.forName("com.mysql.jdbc.Driver");
+            return DriverManager.getConnection(dbURL, dbID, dbPassword);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
+
